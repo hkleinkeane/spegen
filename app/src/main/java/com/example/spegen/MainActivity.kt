f15736cf66211c6ac5d5e6ed15e0ca1e680a4585
Harper Klein Keane <Harperkleinkeane@gmail.com>
2026-05-17 17:30:54 -0400

    Added copyright notices and updated about section in settings to say that the license is: GNU General Public License v3.0.

--- app/src/main/java/com/example/spegen/MainActivity.kt
+++ app/src/main/java/com/example/spegen/MainActivity.kt
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2026 Harper Klein Keane
+ *
+ * This file is part of SpeGen.
+ *
+ * SpeGen is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * SpeGen is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with SpeGen. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.hkleinkeane.spegen
 
 import android.content.Intent
@@ -1756,7 +1768,7 @@ fun AboutContent() {
         Text("SpeGen", fontSize = 24.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.height(8.dp))
         Text("An open-source AAC app developed by Harper Klein Keane. SpeGen's website can be found at the following URL: https://hkleinkeane.github.io/spegen/.", fontSize = 14.sp)
-        Text("License: Apache-2.0", fontSize = 14.sp, color = Color.Gray)
+        Text("License: GNU General Public License v3.0", fontSize = 14.sp, color = Color.Gray)
     }
 }
 
